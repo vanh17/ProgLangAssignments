228da134b0b55a35adca44d60480913a78c24b8b
Haris Skiadas <cskiadas@gmail.com>
2016-01-16 14:27:23 -0500

    Fix tests

--- assignment1tests.ml
+++ assignment1tests.ml
@@ -1,20 +1,20 @@
 (* Add your own tests. Make sure to pay attention to edge cases. *)
-let t1a = fixLastTwo (5, 1, 2) == (5, 1, 2)
-let t1b = fixLastTwo (5, 2, 1) == (5, 1, 2)
+let t1a = fixLastTwo (5, 1, 2) = (5, 1, 2)
+let t1b = fixLastTwo (5, 2, 1) = (5, 1, 2)
 
-let t2a = order (2, 5, 3) == (2, 3, 5)
-let t2b = order (5, 3, 2) == (2, 3, 5)
+let t2a = order (2, 5, 3) = (2, 3, 5)
+let t2b = order (5, 3, 2) = (2, 3, 5)
 
-let t3a = distance (6, 3) == 3
+let t3a = distance (6, 3) = 3
 
-let t4a = greeting (23, "Pete") == "Greetings Pete, you are 23 years old!"
+let t4a = greeting (23, "Pete") = "Greetings Pete, you are 23 years old!"
 
-let t5a = greeting2 (0, "Jackson") == "Greetings Jackson, you are not born yet!"
+let t5a = greeting2 (0, "Jackson") = "Greetings Jackson, you are not born yet!"
 
-let t6a = tooShort (4, "tree") == false
+let t6a = tooShort (4, "tree") = false
 
-let t7a = totalLength ("you", "me") == 5
+let t7a = totalLength ("you", "me") = 5
 
-let t8a = orderedByLengh ("long", "one", "at") == false
+let t8a = orderedByLength ("long", "one", "at") = false
 
 let t9a = prodInRange (3, 5) == true
