1bc647b241f4b5b84342e2b24f728c00e8836164
vanh17 <vanh17@hanover.edu>
2016-02-13 16:14:33 -0500

    subst tests passed. Close #75

--- assignment5tests.ml
+++ assignment5tests.ml
@@ -7,17 +7,20 @@ let t8e = has_vars (Parity (Mul (Add(Int 1, Int 3),Add (Var, Int 2))))
 let t8f = has_vars (Add (Mul (Add (Mul (Add (Mul (Add (Var, Int 2), Mul(Int 2, Int 3)), Int 2), Mul(Int 2, Int 3)), Int 2), Mul(Int 2, Int 3)), Int 2))
 let t8g = not (has_vars (Mul (Mul (Add (Mul (Add (Int 2, Int 2), Mul(Int 2, Int 3)), Int 2), Mul(Int 2, Int 3)), Int 2)))
 
+
 let t9a = count_vars (Add (Var, Int 2)) = 1
 let t9b = count_vars (Add (Int 1, Int 2)) = 0
 let t9c = count_vars (Parity (Mul (Add(Var, Mul (Add(Var, Int 3),Add (Var, Mul (Add(Var, Int 3),Add (Var, Int 2))))),Add (Var, Int 2)))) = 6
 let t9d = count_vars (Add (Mul (Add (Mul (Add (Mul (Add (Var, Int 2), Mul(Int 2, Int 3)), Int 2), Mul(Int 2, Int 3)), Int 2), Mul(Int 2, Int 3)), Int 2)) = 1
 let t9e = count_vars (Mul (Mul (Add (Mul (Add (Int 2, Int 2), Mul(Int 2, Int 3)), Int 2), Mul(Int 2, Int 3)), Int 2)) = 0
 
+
 let t10a = calc_eval (Add (Var, Int 2), 3) = 5
 let t10b = calc_eval (Mul (Mul (Add (Mul (Add (Int 2, Int 2), Mul(Int 2, Int 3)), Int 2), Mul(Int 2, Int 3)), Int 2), 5) = 312
 let t10c = calc_eval (Parity (Mul (Add(Var, Mul (Add(Var, Int 3),Add (Var, Mul (Add(Var, Int 3),Add (Var, Int 2))))),Add (Var, Int 2))), 4) = 0
 let t10d = calc_eval (Parity (Mul (Add(Var, Mul (Add(Var, Int 3),Add (Var, Mul (Add(Var, Int 3),Add (Var, Int 2))))),Add (Var, Int 2))), 3) = 1
 let t10e = calc_eval (Mul (Add(Var, Mul (Add(Var, Int 3),Add (Var, Mul (Add(Var, Int 3),Add (Var, Int 2))))),Add (Var, Int 2)), 3) = 1005
+
 
 let t11a = func_of_calc (Add (Var, Int 2)) 3 = 5
 let t11b = func_of_calc (Mul (Mul (Add (Mul (Add (Int 2, Int 2), Mul(Int 2, Int 3)), Int 2), Mul(Int 2, Int 3)), Int 2)) 5 = 312
@@ -26,11 +29,16 @@ let t11d = func_of_calc (Parity (Mul (Add(Var, Mul (Add(Var, Int 3),Add (Var, Mu
 let t11e = func_of_calc (Mul (Add(Var, Mul (Add(Var, Int 3),Add (Var, Mul (Add(Var, Int 3),Add (Var, Int 2))))),Add (Var, Int 2))) 3 = 1005
 
 
-
 let t12a = subst (Add (Var, Int 1), Mul (Var, Var)) =
                 Mul (Add (Var, Int 1), Add (Var, Int 1))
+let t12b = subst (Mul (Add (Var, Int 2), Mul(Int 2, Int 3)), Mul (Add (Var, Int 2), Mul(Int 2, Int 3))) =
+                Mul (Add (Mul (Add (Var, Int 2), Mul(Int 2, Int 3)), Int 2), Mul(Int 2, Int 3))
+let t12c = subst (subst (Mul (Add (Var, Int 2), Mul(Int 2, Int 3)), Mul (Add (Var, Int 2), Mul(Int 2, Int 3))), Mul (Add (Mul (Add (Var, Int 2), Mul(Int 2, Int 3)), Int 2), Mul(Int 2, Int 3))) =
+                Mul (Add (Mul (Add (Mul (Add (Mul (Add (Var, Int 2), Mul(Int 2, Int 3)), Int 2), Mul(Int 2, Int 3)), Int 2), Mul(Int 2, Int 3)), Int 2), Mul(Int 2, Int 3))
+
 
 let t13a = power 3 = Mul (Mul (Var, Var), Var)
+
 
 let t14a = term (2, 1) = Mul(Int 2, Var)
 
