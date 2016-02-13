793bdc3e2879cffa424b318176808c0643e9860f
vanh17 <vanh17@hanover.edu>
2016-02-13 15:50:25 -0500

    func_of_calc passed all tests. Close #73

--- assignment5tests.ml
+++ assignment5tests.ml
@@ -20,6 +20,12 @@ let t10d = calc_eval (Parity (Mul (Add(Var, Mul (Add(Var, Int 3),Add (Var, Mul (
 let t10e = calc_eval (Mul (Add(Var, Mul (Add(Var, Int 3),Add (Var, Mul (Add(Var, Int 3),Add (Var, Int 2))))),Add (Var, Int 2)), 3) = 1005
 
 let t11a = func_of_calc (Add (Var, Int 2)) 3 = 5
+let t11b = func_of_calc (Mul (Mul (Add (Mul (Add (Int 2, Int 2), Mul(Int 2, Int 3)), Int 2), Mul(Int 2, Int 3)), Int 2)) 5 = 312
+let t11c = func_of_calc (Parity (Mul (Add(Var, Mul (Add(Var, Int 3),Add (Var, Mul (Add(Var, Int 3),Add (Var, Int 2))))),Add (Var, Int 2)))) 4 = 0
+let t11d = func_of_calc (Parity (Mul (Add(Var, Mul (Add(Var, Int 3),Add (Var, Mul (Add(Var, Int 3),Add (Var, Int 2))))),Add (Var, Int 2)))) 3 = 1
+let t11e = func_of_calc (Mul (Add(Var, Mul (Add(Var, Int 3),Add (Var, Mul (Add(Var, Int 3),Add (Var, Int 2))))),Add (Var, Int 2))) 3 = 1005
+
+
 
 let t12a = subst (Add (Var, Int 1), Mul (Var, Var)) =
                 Mul (Add (Var, Int 1), Add (Var, Int 1))
