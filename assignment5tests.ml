5cf26025288823aacda13606c4335938432a659d
vanh17 <vanh17@hanover.edu>
2016-02-13 14:57:55 -0500

    has_vars passed tests. Close #67

--- assignment5tests.ml
+++ assignment5tests.ml
@@ -1,6 +1,11 @@
 (* CALCULATIONS *)
 let t8a = has_vars (Add (Var, Int 2))
 let t8b = not (has_vars (Add (Int 1, Int 2)))
+let t8c = has_vars (Mul (Add (Var, Int 2), Mul(Int 2, Int 3)))
+let t8d = not (has_vars (Add (Add (Int 1, Int 2), Mul (Int 3, Int 6))))
+let t8e = has_vars (Parity (Mul (Add(Int 1, Int 3),Add (Var, Int 2))))
+let t8f = has_vars (Add (Mul (Add (Mul (Add (Mul (Add (Var, Int 2), Mul(Int 2, Int 3)), Int 2), Mul(Int 2, Int 3)), Int 2), Mul(Int 2, Int 3)), Int 2))
+let t8g = not (has_vars (Mul (Mul (Add (Mul (Add (Int 2, Int 2), Mul(Int 2, Int 3)), Int 2), Mul(Int 2, Int 3)), Int 2)))
 
 let t9a = count_vars (Add (Var, Int 2)) = 1
 let t9b = count_vars (Add (Int 1, Int 2)) = 0
