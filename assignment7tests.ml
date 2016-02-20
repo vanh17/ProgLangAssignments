2c369d79f2d8430b77aecedaffd4359bba0e5544
vanh17 <vanh17@hanover.edu>
2016-02-19 22:18:02 -0500

    tabulate passed tests. Close #123

--- assignment7tests.ml
+++ assignment7tests.ml
@@ -3,3 +3,11 @@ let t1b = range1 1 = [1]
 let t1c = range1 2 = [1; 2]
 let t1d = range1 5 = [1; 2; 3; 4; 5]
 let t1e = range1 10 = [1; 2; 3; 4; 5; 6; 7; 8; 9; 10]
+let t1f = range1 (-2) = []
+
+let t2a = tabulate (fun x -> x * x) 5 = [1; 4; 9; 16; 25]
+let t2b = tabulate (fun x -> true) 2 = [true; true]
+let t2c = tabulate (fun x -> ()) 0 = []
+let t2d = tabulate (fun x -> tabulate (fun y -> y * y) x) 4 = [[1]; [1; 4]; [1; 4; 9]; [1; 4; 9; 16]]
+let t2e = tabulate (fun x-> []) (-2) =[]
+
